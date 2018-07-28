a1b8635d0255b2fe2041ddf9e34319658470c186
ewinddc <fallsunshine@foxmail.com>
2018-07-28 14:07:13 +0800

    1. add keystore and sign config 2. implementation the common to build release aar

--- app/src/main/java/com/ewinddc/modulesample/App.java
+++ app/src/main/java/com/ewinddc/modulesample/App.java
@@ -3,13 +3,14 @@ package com.ewinddc.modulesample;
 import android.app.Application;
 import android.content.Intent;
 
+import com.ewinddc.common.BaseApp;
 import com.ewinddc.common.Constants;
 
 /**
  * Created by ewinddc on 2018/4/10.
  */
 
-public class App extends Application{
+public class App extends BaseApp{
 
     @Override
     public void onCreate() {
